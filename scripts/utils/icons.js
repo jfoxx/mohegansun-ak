5e6ddece8be1e4fe694ea544eff3a8765ec75d33
Jeff Foxx <foxx@adobe.com>
2025-10-24 17:10:53 -0400

    not sidebar

--- scripts/utils/icons.js
+++ scripts/utils/icons.js
@@ -5,10 +5,7 @@ const { codeBase } = getConfig();
 export default function loadIcons(icons) {
   for (const icon of icons) {
     const name = icon.classList[1].substring(5);
-    const svg = `<svg class="${icon.className}">
-        <use href="${codeBase}/img/icons/${name}.svg#${name}"></use>
-    </svg>`;
-    icon.insertAdjacentHTML('afterend', svg);
-    icon.remove();
+    const iconPath = `/img/icons/${name}.svg`;
+    icon.style.maskImage = `url(${iconPath})`;
   }
 }
